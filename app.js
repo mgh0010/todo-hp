0311b904bc5b9513fdfad49915169b39899ccacb
Michael <mgh0010@auburn.edu>
2018-06-05 16:48:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist', 'todo-hp')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'todo-hp', 'index.html'));
+});
+const port = process.env.PORT || '3001';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
